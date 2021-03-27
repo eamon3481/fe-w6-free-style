c2688f52f6ba6d6d5faf285da6bdd4edf04ba071
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-03-27 16:21:52 +0900

    [FEAT]Add loginVeiw

--- frontEndDevRoadmap/public/src/loginView.js
+++ frontEndDevRoadmap/public/src/loginView.js
@@ -1,23 +1,71 @@
+import _ from "./components/utils.js";
+
 export default class loginView {
   constructor(target) {
     this.target = target;
+    this.loginSection = target.offsetParent;
+    this.loginInner = this.loginSection.children[1];
   }
 
   init() {
-    this.checkTarget(this.target)
+    this.checkTarget(this.target);
   }
 
   checkTarget(target) {
     if (target.classList[1] === "selected") return;
+    this.switchBtn(target);
+    this.renderView(target);
+  }
+
+  switchBtn(target) {
+    const selected = _.$(".selected", this.loginSection);
+    _.replace(selected, "selected", "unselected");
+    _.replace(target, "unselected", "selected");
+  }
+
+  renderView(target) {
     switch (target.innerText) {
       case "log-in":
-        console.log("login");
-        //this.renderloginView()
+        this.loginInner.innerHTML = this.TemplateLoginView();
         break;
       case "sign-in":
-        console.log("sign");
-        //this.renderSigninView()
+        this.loginInner.innerHTML = this.TemplateSignView();
         break;
     }
+  }
+
+  TemplateLoginView() {
+    return `
+    <form action="/" method="post">
+        <div>
+          <input type="text" id="name" placeholder="Name" />
+        </div>
+        <div>
+          <input type="password" id="password" placeholder="Password" />
+        </div>
+        <button type="submit">Log In</button>
+      </form>
+    `;
+  }
+
+  TemplateSignView() {
+    return ` 
+    <form action="/" method="post">
+    
+    <div>
+      <input type="text" id="name" placeholder="Name" />
+    </div>
+    
+    <div>
+      <input type="password" id="password" placeholder="Password" />
+    </div>
+    
+    <div>
+    <input type="password" id="password2" placeholder="Password" />
+    </div>
+    
+    <button type="submit">Sign In</button>
+  
+    </form>`;
   }
 }
